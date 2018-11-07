c4a56ff3ddba2550660f096785672054f17af4d8
acodexm <adamkx7x@gmail.com>
2018-11-07 10:33:38 +0100

    refactor singleton wrong approach

--- dist/index.js
+++ dist/index.js
@@ -11,22 +11,27 @@ var rex = function rex(str) {
 
 var Singleton = function () {
   var instance;
-  var messages;
+  var messages = {};
+
+  var createInstance = function createInstance() {
+    return function (prefix) {
+      return function (id) {
+        var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+        var result = messages["".concat(prefix ? prefix + '.' : '').concat(id)];
+        Object.keys(options).forEach(function (key) {
+          result = result.replace(rex(key), options[key]);
+        });
+        return result || id;
+      };
+    };
+  };
+
   return {
-    getInstance: function getInstance(translations) {
-      if (translations) messages = translations;
+    getInstance: function getInstance(data) {
+      if (data) messages = data;
 
       if (!instance) {
-        return function (prefix) {
-          return function (id) {
-            var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-            var result = messages["".concat(prefix ? prefix + '.' : '').concat(id)];
-            Object.keys(options).forEach(function (key) {
-              result = result.replace(rex(key), options[key]);
-            });
-            return result || id;
-          };
-        };
+        instance = createInstance();
       }
 
       return instance;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ const rex = str => new RegExp(`{${str}}`, 'g');
 
 export const Singleton = (() => {
     let instance;
-    let messages;
+    let messages = {};
+    const createInstance = () => {
+        return (prefix) => (id, options = {}) => {
+            let result = messages[ `${prefix ? prefix + '.' : ''}${id}` ];
+            Object.keys(options).forEach((key) => {
+                result = result.replace(rex(key), options[ key ]);
+            });
+            return result || id;
+        };
+    };
 
     return {
-        getInstance: translations => {
-            if (translations) messages = translations;
+        getInstance(data) {
+            if (data) messages = data;
             if (!instance) {
-                return prefix => (id, options={}) => {
-                    let result = messages[ `${prefix ? prefix + '.' : '' }${id}` ];
-                    Object.keys(options).forEach(key => {
-                        result = result.replace(rex(key), options[ key ]);
-                    });
-                    return result || id;
-                };
+                instance = createInstance();
             }
             return instance;
         },
