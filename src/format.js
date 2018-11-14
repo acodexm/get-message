de1847f5a0c61fe746bffe3598182e38743c9e5e
acodexm <adamkx7x@gmail.com>
2018-11-14 10:22:47 +0100

    added yahoo copyright notice

--- src/format.js
+++ src/format.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
 import IntlRelativeFormat from 'intl-relativeformat';
 import React, { createElement, isValidElement } from 'react';
 import invariant from 'invariant';
--- src/types.js
+++ src/types.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
 import PropTypes from 'prop-types';
 
 const { bool, number, string, func, object, oneOf, shape, any, oneOfType } = PropTypes;
